a7f11fa40f397c3ba2f2931834c65916abd73da4
User <user@SDAcourse.ee>
2022-11-28 19:37:13 +0200

    Initial commit

---
+++ src/main/java/products/Product.java
@@ -0,0 +1,39 @@
+package products;
+
+public class Product {
+    //Name, Quantity, Description, Price
+
+    public String name;
+    public int Quantity;
+    public String Description;
+    public float Price;
+
+    public Product(String name, int quantity, String description, float price){
+        this.name = name;
+        this.Quantity = quantity;
+        this.Description = description;
+        this.Price = price;
+
+        //public String toString(); {
+       //     return "Name: " + name + ", quantity: " + quantity + ", description: " + description + ", price" + price;
+
+
+            //   public int getQuantity() {
+     //       return quantity;
+     //   }
+
+   //    public void setQuantity(int quantity) {
+            //this.Quantity = quantity;
+   //     }
+   //     public int getPrice() {
+    //        return price;
+   //     }
+
+   //     public void setPrice(int price) {
+  //          this.Price = price;
+  //      }
+
+
+
+        }
+    }
---
+++ src/main/java/products/Supermarket.java
@@ -0,0 +1,64 @@
+package products;
+
+import java.util.Scanner;
+
+
+    public class Main {
+        public static void main(String[] args) {
+            System.out.println("Welcome to supermarket inventory program!");
+
+            Scanner scanner = new Scanner(System.in);
+            SuperMarket supermarket = new SuperMarket();
+
+            String input = "";
+            do {
+                System.out.println("""
+                    Lets start inventory today!
+                                        
+                    1. Add product
+                    2. View all products
+                    3. Remove a product
+                    4. View single product
+                    5. Buy a product (deducting the products quantity)
+                    6. Track the total amount earned by the supermarket
+                                        
+                    """);
+                input = scanner.nextLine();
+                switch (input){
+                    case "quit":
+                        System.out.println("Exit application");
+                        break;
+                    case "1":
+                        //System.out.println("Add a product");
+                        supermarket.addProduct();
+                        break;
+                    case "2":
+                        //System.out.println("View all products");
+                        supermarket.viewAllProducts();
+                        break;
+                    case "3":
+                        //System.out.println("Remove a product");
+                        supermarket.removeProduct();
+                        break;
+                    case "4":
+                        //System.out.println("View a single product");
+                        supermarket.viewProduct();
+                        break;
+                    case "5":
+                        System.out.println("Buy a product");
+                        break;
+                    case "6":
+                        System.out.println("Track total amount earned by supermarket");
+                        break;
+                    default:
+                        System.out.println("Please select a valid option");
+                        break;
+
+
+                }
+            } while (!input.equals("quit"));
+        }
+    }
+
+
+}
